831c01d6fae7d1471fae7dcb802087f53b6966c3
Mirai3103 <huuhoag1412@gmail.com>
2025-06-01 12:00:28 +0700

    add jwt

--- src/main/java/com/example/FoodHKD/config/SecurityConfig.java
+++ src/main/java/com/example/FoodHKD/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.FoodHKD.config;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 @Configuration
 @EnableWebSecurity
+@Order(2)
 public class SecurityConfig {
 
     @Autowired
